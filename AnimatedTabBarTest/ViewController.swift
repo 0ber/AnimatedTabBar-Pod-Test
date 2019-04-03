9f2ccc9645eab9c44d82b17552fe9c9b4c4a735e
Alex K <kalinkin.aleksey@gmail.com>
2019-04-03 11:57:15 +0300

    add project

---
+++ AnimatedTabBarTest/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  AnimatedTabBarTest
+//
+//  Created by Alex K on 03/04/2019.
+//  Copyright © 2019 Ramotion. All rights reserved.
+//
+
+import UIKit
+import RAMAnimatedTabBarController
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
