60219c91f80e629c4143610d20daf2f4862fd2e7
Alex K <kalinkin.aleksey@gmail.com>
2019-04-10 10:27:15 +0300

    set animation programmatically

--- AnimatedTabBarTest/AppDelegate.swift
+++ AnimatedTabBarTest/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import RAMAnimatedTabBarController
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        
+        let myTabBar = self.window?.rootViewController as! RAMAnimatedTabBarController
+        let item = myTabBar.animatedItems[0] as! RAMAnimatedTabBarItem
+//            myTabBar.animatedItems[0].animation = RAMBounceAnimation()
+        item.animation = RAMRotationAnimation()
+        
+            
         return true
     }
 
